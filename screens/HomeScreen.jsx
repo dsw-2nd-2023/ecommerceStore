3e01ca0d1f9fbb5add3a24021da226c648e68175
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-26 18:52:23 +0200

    Get products

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -7,16 +7,59 @@ import {
   Pressable,
   Dimensions,
   StyleSheet,
+  Alert,
+  ToastAndroid,
 } from "react-native";
-import { useContext } from "react";
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import products from "../Products";
+//import products from "../Products";
 import { CartContext } from "../CartCxt";
+
+import { db } from "../firebase";
+import { collection, getDocs, query, onSnapshot } from "firebase/firestore";
+import { useFocusEffect } from "@react-navigation/native";
 
 const HomeScreen = ({ navigation }) => {
   const { addToCart } = useContext(CartContext);
+
+  const [products, setProducts] = useState([]);
+
+  useFocusEffect(() => {
+    //getProducts()
+  })
+
+  useEffect(() => {
+    const q = query(collection(db, "products"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data(), price: `R ${doc.data().price}` })
+      });
+      setProducts(data)
+    });
+
+    ()=> unsubscribe()
+    
+  }, [])
+
+
+  const getProducts = async () => {
+    const querySnapshot = await getDocs(collection(db, "products"));
+
+    if (querySnapshot.empty) {
+      //ToastAndroid.showWithGravity("No data", ToastAndroid.SHORT, 15)
+      return Alert.alert("No data")
+    }
+    let data = []
+    querySnapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() })
+    });
+
+    setProducts(data)
+    //console.log(data);
+  }
 
   return (
     <View style={styles.container}>
@@ -69,6 +112,7 @@ const HomeScreen = ({ navigation }) => {
         )}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => <Text>No Data</Text>}
       />
     </View>
   );
@@ -100,7 +144,7 @@ const styles = StyleSheet.create({
       width: "100%",
       paddingHorizontal: 10,
     },
-  },
+  }
 });
 
 export default HomeScreen;
