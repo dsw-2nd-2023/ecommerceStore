7f12b19542febd5a3962771af1f2dacc4d978820
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-26 20:47:31 +0200

    add new product

--- screens/AddProduct.jsx
+++ screens/AddProduct.jsx
@@ -1,64 +1,155 @@
-
-import { useContext } from "react";
+import { db } from "../firebase";
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
-import { CartContext } from "../CartCxt";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { Alert, Button, Dimensions, Image, StyleSheet, Text, TextInput, ToastAndroid, View } from "react-native";
 
-const AddProduct = ({ navigation, route }) => {
-  const { product, from = "HomeScreen" } = route.params;
-  
-  const { addToCart } = useContext(CartContext);
+const AddProduct = () => {
+
+  const [newProduct, setnewProduct] = useState({
+    name: "", description: "", image: "", price: "0"
+  })
+
+  const addProduct = async () => {
+    try {
+      const newProductRef = doc(collection(db, "products"))
+      const data = await setDoc(newProductRef, { ...newProduct, price: parseInt(newProduct.price) })
+      //console.log(data);
+      ToastAndroid.show("Added new product ", ToastAndroid.SHORT)
+      setnewProduct({
+        name: "", description: "", image: "", price: "0"
+      })
+    } catch (error) {
+      Alert.alert("Error saving!", JSON.stringify(error))
+      //console.error(error)
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
       <View style={styles.container.details}>
-        <Text>Add Product</Text>
+        <View style={styles.inpGroup}>
+          <Text>Name:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nike Air-Force 2"
+            onChangeText={(text) => setnewProduct((prev) => ({ ...prev, name: text }))}
+            value={newProduct.name}
+          />
+        </View>
+        <View style={styles.inpGroup}>
+          <Text>Description:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="This is the best shoe ever"
+            onChangeText={(text) => setnewProduct((prev) => ({ ...prev, description: text }))}
+            value={newProduct.description}
+          />
+        </View>
+        <View style={styles.inpGroup}>
+          <Text>Price:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="4000"
+            onChangeText={(text) => setnewProduct((prev) => ({ ...prev, price: text }))}
+            value={newProduct.price}
+          />
+        </View>
+        <View style={styles.inpGroup}>
+          <Text>Image:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="https://console.firebase.google.com/u/0.jpg"
+            onChangeText={(text) => setnewProduct((prev) => ({ ...prev, image: text }))}
+            value={newProduct.image}
+          />
+        </View>
+        <View style={styles.inpGroup}>
+          <Button title="Save" onPress={addProduct} />
+        </View>
       </View>
+      {/* <View style={styles.product.details}>
+        <Image source={{ uri: newProduct.image !== "" ? newProduct.image : "https://placehold.co/600x400" }} style={styles.product.image} />
+        <View style={styles.product.details}>
+          <View
+            style={{
+              marginBottom: 10,
+              padding: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              {newProduct.name}
+            </Text>
+            <Text
+              style={{
+                fontSize: 18,
+              }}
+            >
+              {newProduct.price}
+            </Text>
+          </View>
+          <MaterialCommunityIcons
+            name="cart-arrow-down"
+            size={30}
+            color="green"
+            style={{
+              padding: 10,
+            }}
+          />
+        </View>
+      </View> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingHorizontal: 30,
     backgroundColor: "#fff",
-    position: "relative",
+    flex: 1,
+    paddingTop: 20,
+    flexDirection: "column",
     details: {
-      position: "absolute",
-      top: 40,
-      left: 20,
-      zIndex: 100,
-      flexDirection: "row",
-      alignItems: "center",
+      flexDirection: "column",
     },
   },
-  textVisisble: {
-    textShadowColor: "grey",
-    textShadowRadius: 1,
-    fontSize: 30,
-    fontWeight: "bold",
-    marginLeft: 10,
-    color: "white",
+  inpGroup: {
+    paddingVertical: 10
   },
-  details: {
-    alignItems: "center",
-    width: "100%",
+  input: {
+    borderColor: "grey",
+    borderWidth: 0.5,
+    padding: 5,
     paddingHorizontal: 10,
+    marginTop: 5
   },
-  description: {
-    fontSize: 18,
-    fontWeight: "normal",
-    textAlign: "center",
-    color: "grey",
-    marginVertical: 10,
-  },
-  image: {
-    width: "100%",
-    height: "65%",
-    marginBottom: 20,
-  },
+  product: {
+    component: {
+      borderWidth: 1,
+      borderColor: "lightgrey",
+      marginBottom: 15,
+      width: Dimensions.get("window").width,
+    },
+    image: {
+      width: "100%",
+      height: 280,
+      marginBottom: 20,
+    },
+    details: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignnewProducts: "center",
+      width: "100%",
+      paddingHorizontal: 10,
+    },
+  }
 });
 
 export default AddProduct;
